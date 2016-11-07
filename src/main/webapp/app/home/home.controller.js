2c973bca883b393671a91cbab8bbdeb3a0eea30e
Matthew Zackschewski <mzack5020@gmail.com>
2016-11-07 01:33:34 -0500

    Added services query to home page

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,25 +5,32 @@
         .module('eLancerApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'Services', 'LoginService', '$state'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController ($scope, Principal, Services, LoginService, $state) {
         var vm = this;
 
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
+        vm.services = [];
         $scope.$on('authenticationSuccess', function() {
             getAccount();
         });
 
         getAccount();
+        getServices();
 
         function getAccount() {
             Principal.identity().then(function(account) {
                 vm.account = account;
                 vm.isAuthenticated = Principal.isAuthenticated;
+            });
+        }
+        function getServices() {
+            Services.query(function(result) {
+                vm.services = result;
             });
         }
         function register () {
